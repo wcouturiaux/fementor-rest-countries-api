1f522ebe2af59e8d2c56d857ffb344df3981055b
Will C <wlccodes@gmail.com>
2022-03-12 23:19:10 -0500

    Remove unused imports

--- fementor-rest-countries-api-app/src/webpages/index.js
+++ fementor-rest-countries-api-app/src/webpages/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import {
     BrowserRouter as Router,
-    Switch,
     Route,
-    Link,
     Routes
 } from "react-router-dom";
 
-import Nav from "./nav";
 import Home from './home';
 import Details from './details';
 
