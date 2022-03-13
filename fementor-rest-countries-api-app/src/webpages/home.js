f1af247f2e33aae6acee54ab595cc7019e7a3c2c
Will C <wlccodes@gmail.com>
2022-03-12 22:23:52 -0500

    Add fetch and display country names

--- fementor-rest-countries-api-app/src/webpages/home.js
+++ fementor-rest-countries-api-app/src/webpages/home.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [countries, setCountries] = useState([]);
 
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setCountries(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return(
+            <div>
+                <ul>
+                    {countries.map(country =>(
+                        <li key={country.cca3}>
+                            {country.name.common}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Home;
