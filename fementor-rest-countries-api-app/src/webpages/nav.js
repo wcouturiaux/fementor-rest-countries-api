2118115e57bb6a5a331a45138d8a34c4c64923d4
Will C <wlccodes@gmail.com>
2022-03-12 20:55:38 -0500

    Add components/pages

---
+++ fementor-rest-countries-api-app/src/webpages/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+
+    return(
+        <nav>
+            <h1>Where in the world?</h1>
+            <button className="mode">MODE</button>
+        </nav>
+    );
+}
+
+export default Nav;
