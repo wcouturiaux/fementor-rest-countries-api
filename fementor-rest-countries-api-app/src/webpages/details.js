f65a1e37732cfd178d9c112a8ecd10d146957f38
Will C <wlccodes@gmail.com>
2022-03-15 00:48:43 -0400

    Configure details page

--- fementor-rest-countries-api-app/src/webpages/details.js
+++ fementor-rest-countries-api-app/src/webpages/details.js
@@ -12,6 +12,19 @@ const Details = () => {
                 alt={'Flag of ' + country.name.common}
             >
             </img>
+            <div className='country-details'>
+                <h2>{country.name.common}</h2>
+                <p>Native Name: {country.name.official}</p>
+                <p>Population: {country.population}</p>
+                <p>Region: {country.region}</p>
+                <p>Sub Region: {country.subregion}</p>
+                <p>Capital: {country.capital}</p>
+                <p>Top Level Domain: {country.tld}</p>
+                <p>Currency: {Object.keys(country.currencies).map(key=>(country.currencies[key].name+", "))}</p>
+            </div>
+            <div className='country-details' id='border-countries'>
+                <p>Border Countries: {country.borders.map(border=>(border + "\t"))}</p>
+            </div>
         </div>
     );
 }
