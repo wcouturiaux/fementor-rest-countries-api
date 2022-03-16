bc0117c2d2b6a952c37b654b0ed196867f6d603e
Will C <wlccodes@gmail.com>
2022-03-15 23:20:36 -0400

    Update details css

--- fementor-rest-countries-api-app/src/webpages/details.js
+++ fementor-rest-countries-api-app/src/webpages/details.js
@@ -17,16 +17,16 @@ const Details = () => {
             </img>
             <div className='country-details'>
                 <h2>{country.name.common}</h2>
-                <p>Native Name: {country.name.official}</p>
-                <p>Population: {country.population}</p>
-                <p>Region: {country.region}</p>
-                <p>Sub Region: {country.subregion}</p>
-                <p>Capital: {country.capital}</p>
-                <p>Top Level Domain: {country.tld}</p>
-                <p>Currency: {Object.keys(country.currencies).map(key=>(country.currencies[key].name+", "))}</p>
+                <p><span>Native Name: </span>{country.name.official}</p>
+                <p><span>Population: </span>{country.population}</p>
+                <p><span>Region: </span>{country.region}</p>
+                <p><span>Sub Region: </span>{country.subregion}</p>
+                <p><span>Capital: </span>{country.capital}</p>
+                <p><span>Top Level Domain: </span>{country.tld}</p>
+                <p><span>Currency: </span>{Object.keys(country.currencies).map(key=>(country.currencies[key].name+", "))}</p>
             
                 <div className='borders' id='border-countries'>
-                    <p>Border Countries: {country.borders.map(border=>(border + "\t"))}</p>
+                    <p><span>Border Countries: </span>{country.borders.map(border=>(border + "\t"))}</p>
                 </div>
             </div>
         </div>
