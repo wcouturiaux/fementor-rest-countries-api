abb1a8ebd6806c2005068112f93ad9ef261cdfc0
Will C <wlccodes@gmail.com>
2022-03-23 23:40:51 -0400

    Add languages and extra divs

--- fementor-rest-countries-api-app/src/webpages/details.js
+++ fementor-rest-countries-api-app/src/webpages/details.js
@@ -17,14 +17,18 @@ const Details = () => {
             </img>
             <div className='country-details'>
                 <h2>{country.name.common}</h2>
-                <p><span>Native Name: </span>{country.name.official}</p>
-                <p><span>Population: </span>{country.population}</p>
-                <p><span>Region: </span>{country.region}</p>
-                <p><span>Sub Region: </span>{country.subregion}</p>
-                <p><span>Capital: </span>{country.capital}</p>
-                <p><span>Top Level Domain: </span>{country.tld}</p>
-                <p><span>Currency: </span>{Object.keys(country.currencies).map(key=>(country.currencies[key].name+", "))}</p>
-            
+                <div className='country-details-1'>
+                    <p><span>Native Name: </span>{country.name.official}</p>
+                    <p><span>Population: </span>{country.population}</p>
+                    <p><span>Region: </span>{country.region}</p>
+                    <p><span>Sub Region: </span>{country.subregion}</p>
+                    <p><span>Capital: </span>{country.capital}</p>
+                </div>
+                <div className='country-details-2'>
+                    <p><span>Top Level Domain: </span>{country.tld}</p>
+                    <p><span>Currency: </span>{Object.keys(country.currencies).map(key=>(country.currencies[key].name+", "))}</p>
+                    <p><span>Languages: </span>{Object.keys(country.languages).map(key=>(country.languages[key] + ", "))}</p>
+                </div>
                 <div className='borders' id='border-countries'>
                     <p><span>Border Countries: </span>{country.borders.map(border=>(border + "\t"))}</p>
                 </div>
